c18640ff5e6a224ba995b9cc9fa6c0c20b539958
GhzGarage <57848836+GhzGarage@users.noreply.github.com>
2021-04-05 20:45:46 -0500

    fxmanifest and dependency

---
+++ qb-ambulancejob/fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-AmbulanceJob'
+version '1.0.0'
+
+client_scripts {
+	'@PolyZone/client.lua',
+	'config.lua',
+	'client/main.lua',
+	'client/wounding.lua',
+	'client/job.lua',
+	'client/dead.lua',
+	'client/gui.lua',
+}
+
+server_scripts {
+	'config.lua',
+	'server/main.lua',
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+files {
+	'interiorproxies.meta'
+}
+
+dependencies {
+	'qb-core',
+	'PolyZone'
+}
