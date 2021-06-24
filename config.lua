e418e15d5af3d5fd111a97c88e86c7fd8d705f38
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-06-24 14:01:29 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -27,14 +27,14 @@ Config.Locations = {
         [1] = vector4(332.51, -595.74, 43.28, 76.0),
     },        
     ["beds"] = {
-        [1] = {vector4(311.13, -582.89, 43.53, 335.65), taken = false, model = 1631638868},
-        [2] = {vector4(313.96, -579.05, 43.53, 164.5), taken = false, model = 1631638868},
-        [3] = {vector4(314.58, -584.09, 43.53, 335.65), taken = false, model = 1631638868},
-        [4] = {vector4(317.74, -585.29, 43.53, 335.65), taken = false, model = 1631638868},
-        [5] = {vector4(319.47, -581.04, 43.53, 164.5), taken = false, model = 1631638868}, 
-        [6] = {vector4(366.43, -581.54, 43.28, 66.5), taken = false, model = 1631638868}, 
-        [7] = {vector4(364.93, -585.86, 43.28, 67.5), taken = false, model = 1631638868}, 
-        [8] = {vector4(363.82, -589.09, 43.28, 68.5), taken = false, model = 1631638868},
+        [1] = {coords = vector4(311.13, -582.89, 43.53, 335.65), taken = false, model = 1631638868},
+        [2] = {coords = vector4(313.96, -579.05, 43.53, 164.5), taken = false, model = 1631638868},
+        [3] = {coords = vector4(314.58, -584.09, 43.53, 335.65), taken = false, model = 1631638868},
+        [4] = {coords = vector4(317.74, -585.29, 43.53, 335.65), taken = false, model = 1631638868},
+        [5] = {coords = vector4(319.47, -581.04, 43.53, 164.5), taken = false, model = 1631638868}, 
+        [6] = {coords = vector4(366.43, -581.54, 43.28, 66.5), taken = false, model = 1631638868}, 
+        [7] = {coords = vector4(364.93, -585.86, 43.28, 67.5), taken = false, model = 1631638868}, 
+        [8] = {coords = vector4(363.82, -589.09, 43.28, 68.5), taken = false, model = 1631638868},
     }
 }
 
