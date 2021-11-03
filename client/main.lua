4d8f7f18ba3617e086389ec132ad7fedc3cdbfb0
Tony <1414927+zfbx@users.noreply.github.com>
2021-11-03 16:41:57 -0400

    Fix for #81

--- client/main.lua
+++ client/main.lua
@@ -726,6 +726,9 @@ end)
 
 RegisterNetEvent('hospital:client:RespawnAtHospital', function()
     TriggerServerEvent("hospital:server:RespawnAtHospital")
+    if exports["qb-policejob"].IsHandcuffed() then
+        TriggerEvent("police:client:GetCuffed", -1)
+    end
     TriggerEvent("police:client:DeEscort")
 end)
 
