98104925d9ce900f404787c7b7e1e7a382790289
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-05-01 13:01:30 +0200

    fix(client/job): return string instead of table

--- client/job.lua
+++ client/job.lua
@@ -203,7 +203,7 @@ RegisterNetEvent('hospital:client:CheckStatus', function()
                             TriggerEvent('chat:addMessage', {
                                 color = { 255, 0, 0},
                                 multiline = false,
-                                args = {Lang:t('info.status'), QBCore.Shared.Weapons[v]}
+                                args = {Lang:t('info.status'), QBCore.Shared.Weapons[v].damagereason}
                             })
                         end
                     elseif result["BLEED"] > 0 then
