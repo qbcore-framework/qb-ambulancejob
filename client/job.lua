f82834c4489cda2e87de2002224714bc481f618e
SKITTLE <92496158+SKITTLE6969@users.noreply.github.com>
2023-02-25 00:26:31 +0530

    Update job.lua
    why though? knealing down to heal near beds

--- client/job.lua
+++ client/job.lua
@@ -224,7 +224,7 @@ RegisterNetEvent('hospital:client:RevivePlayer', function()
             }, {
                 animDict = healAnimDict,
                 anim = healAnim,
-                flags = 33,
+                flags = 16,
             }, {}, {}, function() -- Done
                 StopAnimTask(PlayerPedId(), healAnimDict, "exit", 1.0)
                 QBCore.Functions.Notify(Lang:t('success.revived'), 'success')
@@ -255,7 +255,7 @@ RegisterNetEvent('hospital:client:TreatWounds', function()
             }, {
                 animDict = healAnimDict,
                 anim = healAnim,
-                flags = 33,
+                flags = 16,
             }, {}, {}, function() -- Done
                 StopAnimTask(PlayerPedId(), healAnimDict, "exit", 1.0)
                 QBCore.Functions.Notify(Lang:t('success.helped_player'), 'success')
